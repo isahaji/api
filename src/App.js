3c36ac9156715894e73dde7fe4867eaa5689302a
isahaji <isa@isahaji.com>
2021-10-13 14:54:28 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Map, Marker } from "pigeon-maps";
+import ddata from "./UserList";
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function App() {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/users").then((e) => {
+      setData(e.data);
+      console.log(data);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={0}>
+        {ddata.map((e) => {
+          return (
+
+          <Marker width={50} anchor={[e.address.geo.lat,e.address.geo.lng]} />
+          
+          )
+
+        })}
+      </Map>
+
+      {ddata.map((e) =>  {
+        return( 
+          <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {e.name}
+        </Typography>
+        <Typography variant="h5" component="div">
+
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {e.username} | {e.email}
+        </Typography>
+       
+      </CardContent>
+      <CardActions>
+      <Card sx={{ minWidth: 275 }}>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button href={e.phone} size="small">Phone</Button>
+    <Button size="small" href={e.website}>Website</Button>
+      </CardActions>
+    </Card>
+      </CardActions>
+    </Card>
+        )
+      })}
+      
     </div>
   );
 }
